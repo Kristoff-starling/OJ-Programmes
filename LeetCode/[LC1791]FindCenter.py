2be6e924aafc0001664372383f11a5861a6aff4e
starling <2227547142@qq.com>
2022-02-18 18:52:13 +0800

    [ADD] LC1791

---
+++ LeetCode/[LC1791]FindCenter.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        if edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
+            return edges[0][0]
+        else:
+            return edges[0][1]
