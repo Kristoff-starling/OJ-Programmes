01111027152629060e7142fb5b94e2c98893b142
starling <2227547142@qq.com>
2022-06-17 14:10:48 +0800

    [MODIFY] LC1089 A cleverer implementation

--- LeetCode/[LC1089]DuplicateZero.py
+++ LeetCode/[LC1089]DuplicateZero.py
@@ -1,7 +1,14 @@
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
-        tmp, n = [], len(arr)
-        for num in arr:
-            if num != 0: tmp.append(num)
-            else: tmp.append(num); tmp.append(num)
-        for i in range(n): arr[i] = tmp[i]
+        pt1, pt2, n = -1, 0, len(arr)
+        while pt2 < n:
+            pt1 += 1
+            if arr[pt1] == 0: pt2 += 2
+            else: pt2 += 1
+        while pt1 >= 0:
+            pt2 -= 1
+            if pt2 < n: arr[pt2] = arr[pt1]
+            if arr[pt1] == 0:
+                pt2 -= 1
+                if pt2 < n: arr[pt2] = arr[pt1]
+            pt1 -= 1
