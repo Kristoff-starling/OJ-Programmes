157f283d3cf636591b882e3d6b92ae6f4eda488c
Yuyao Wang <yuyao6@cs.washington.edu>
2024-06-24 00:38:40 +0800

    refactor: move problem solving programs to another repo

--- NJU-ProblemSolving/2020/Problem Solving I - 01/循环.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main ()
-{
-    int a,b;
-    while (scanf("%d%d",&a,&b) && !(a==0 && b==0)) printf("%lld\n",(long long)(a)+b);
-    return 0;
-}
